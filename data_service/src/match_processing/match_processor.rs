33d3eec4b0125481a00aff35497b5b6db38a0467
Barun Parruck <barun.parruck@gmail.com>
2024-04-24 22:52:18 +0100

    Add get_match and get_match_timeline (#11)
    * Add get_match and get_match_timeline
    
    * Add constructor
    
    * Rename variable

---
+++ data_service/src/match_processing/match_processor.rs
@@ -0,0 +1,37 @@
+use riven::{consts::RegionalRoute, models::match_v5::MatchTimeline, RiotApi};
+
+struct MatchProcessor {
+    riot_api: RiotApi,
+}
+
+impl MatchProcessor {
+    pub async fn new(api_key: String) -> Self {
+        Self {
+            riot_api: RiotApi::new(api_key),
+        }
+    }
+
+    pub async fn get_match(
+        &self,
+        match_id: &str,
+        regional_route: RegionalRoute,
+    ) -> Option<riven::models::match_v5::Match> {
+        self.riot_api
+            .match_v5()
+            .get_match(regional_route, match_id)
+            .await
+            .unwrap()
+    }
+
+    pub async fn get_match_timeline(
+        &self,
+        match_id: &str,
+        regional_route: RegionalRoute,
+    ) -> Option<MatchTimeline> {
+        self.riot_api
+            .match_v5()
+            .get_timeline(regional_route, match_id)
+            .await
+            .unwrap()
+    }
+}
