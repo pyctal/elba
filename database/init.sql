300babc0fb10b96ca758d5dcb8a50c9acb825ed5
pmohanty <pmohanty@terpmail.umd.edu>
2024-04-25 18:47:39 -0400

    Fixed skill issue

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE elba;
+\connect elba;
 
 CREATE TABLE match_data
-(MATCH_ID varchar, MATCH_START_TIME bigint,PROCESSED boolean)
+(MATCH_ID varchar, MATCH_START_TIME bigint,PROCESSED boolean);
