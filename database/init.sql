2ece749d3e9b9fd60034ad905fe02c2a56dd3591
Barun Parruck <barun.parruck@gmail.com>
2024-04-28 00:43:27 +0100

    Adds diesel stuff

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE elba;
 \connect elba;
 
-CREATE TABLE match_data
-(MATCH_ID varchar, MATCH_START_TIME bigint,PROCESSED boolean);
+-- CREATE TABLE match_data
+-- (MATCH_ID varchar, MATCH_START_TIME bigint,PROCESSED boolean);
