a5ec9c5095bb352ba2dab33fed439c5f5e5ab4e7
Barun Parruck <barun.parruck@gmail.com>
2024-04-29 03:22:06 +0100

    Forget about champion

--- data_service/src/types.rs
+++ data_service/src/types.rs
@@ -1,13 +1,4 @@
 pub struct PuuidToChampionMapping {
     puuid: String,
-    champion: Champion,
-}
-
-pub enum Champion {
-    Aatrox,
-    Ahri,
-    Akali,
-    Alistar,
-    Amumu,
-    Vayne,
+    champion: String,
 }
